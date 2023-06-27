00ab350c81f0bc994c16c59ed93c2f9bc5c00d87
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-06-26 20:41:38 -0700

    set on click event

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ resultBox = document.querySelector(".result-box"),
 wonText = resultBox.querySelector(".won-text"),
 replayBtn = resultBox.querySelector("button");
 
+window.onload = ()=>{
+    for (let i = 0; i < allBox.length; i++) {
+       allBox[i].setAttribute("onclick", "clickedBox(this)");
+    }
+}
+
